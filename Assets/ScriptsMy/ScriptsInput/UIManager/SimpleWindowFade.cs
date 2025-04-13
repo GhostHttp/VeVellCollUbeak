fa32f266e4c4a00cb0897210af5a4394c9879087
FixBoy <helldogrunner@gmail.com>
2025-04-13 13:14:21 +0300

    some changes

--- Assets/ScriptsMy/ScriptsInput/UIManager/SimpleWindowFade.cs
+++ Assets/ScriptsMy/ScriptsInput/UIManager/SimpleWindowFade.cs
@@ -9,24 +9,24 @@ public class SimpleWindowFade : MonoBehaviour
 
     public void PanelFadeIn()
     {
-        //_canvasGroup.alpha = 0f;
-        //_rectTransform.transform.localPosition = new Vector3(0f, -1000f, 0f);
-        //_rectTransform.transform.localScale = new Vector3(0f, 0, 0f);
-       //_rectTransform.DOAnchorPos(new Vector2(0f, 0f),_fadeTime,false).SetEase(Ease.OutElastic);
+        _canvasGroup.alpha = 0f;
+        _rectTransform.transform.localPosition = new Vector3(0f, -1000f, 0f);
+        _rectTransform.transform.localScale = new Vector3(0f, 0, 0f);
+        _rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutElastic);
 
-        //_rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutSine);
-        //_rectTransform.DOScale(new Vector3(1f,1f,1f), _fadeTime).SetEase(Ease.InFlash);
-        //_canvasGroup.DOFade(1, _fadeTime);
+        _rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutSine);
+        _rectTransform.DOScale(new Vector3(1f, 1f, 1f), _fadeTime).SetEase(Ease.InFlash);
+        _canvasGroup.DOFade(1, _fadeTime);
     }
 
     public void PanelFadeOut()
     {
-        //_canvasGroup.alpha = 1f;
-        //_rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
-        // _rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutElastic);
-        //_rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutSine);
-        //_rectTransform.DOScale(new Vector3(0f, 0f, 0f), _fadeTime).SetEase(Ease.InOutFlash);
-        //_canvasGroup.DOFade(0, _fadeTime);
+        _canvasGroup.alpha = 1f;
+        _rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
+        _rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutElastic);
+        _rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutSine);
+        _rectTransform.DOScale(new Vector3(0f, 0f, 0f), _fadeTime).SetEase(Ease.InOutFlash);
+        _canvasGroup.DOFade(0, _fadeTime);
     }
 
 }
